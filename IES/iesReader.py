946e5752840a2979b61bbdc53ec19c57d2ca02f9
RyotoTomioka <tommy.ryoto93@gmail.com>
2016-06-21 02:46:41 +0900

    IESファイルの読み込みはOK

--- IES/iesReader.py
+++ IES/iesReader.py
@@ -4,9 +4,14 @@ import csv
 
 class IESreader:
     # IESファイルのパス
-    iesFile = ""
+    iesFile = "IES/profile.ies"
+    # データ部分でアングルが記載されている行
+    iesAngleLine = 14
+    # データ部分で光度が記載されている行
+    iesDataLine = 16
 
     def __init__(self):
+        self.profile = ""
         self.manufac = ""
         self.test = ""
         self.date = ""
@@ -14,18 +19,51 @@ class IESreader:
         self.lamp = ""
         self.lampcat = ""
         self.luminaire = ""
+        self.tilt = ""
+        self.angles = []
+        self.data = []
+
+        self.readCSV()
 
     def readCSV(self):
+        # ヘッダ以降のデータが始まる行
+        start_line = 0
+
         f = open(IESreader.iesFile, 'r')
-        reader = csv.reader(f)
-        next(reader)  # ヘッダを読み飛ばす
+        lines = f.readlines()
 
-        
+        self.profile = lines[0][6:]
 
-        # 装置の準備
-        for var in range(0, Initial.light):
-            Initial.lightList.append(Light())
+        print(self.profile)
 
-        for var in range(0, Initial.sensor):
-            Initial.sensorList.append(Sensor())
-            Initial.sensorList[var].set_influence(next(reader))
+        # ヘッダ部分を読み込む
+
+        i = 1
+        while True:
+            if lines[i].find("[MANUFAC]") > -1:
+                self.manufac = lines[i][9:]
+            elif lines[i].find("[TEST]") > -1:
+                self.test = lines[i][6:]
+            elif lines[i].find("[DATE]") > -1:
+                self.test = lines[i][6:]
+            elif lines[i].find("[LUMCAT]") > -1:
+                self.test = lines[i][8:]
+            elif lines[i].find("[LAMP]") > -1:
+                self.test = lines[i][6:]
+            elif lines[i].find("[LAMPCAT]") > -1:
+                self.test = lines[i][8:]
+            elif lines[i].find("[LUMINAIRE]") > -1:
+                self.test = lines[i][11:]
+            elif lines[i].find("TILT") > -1:
+                self.test = lines[i][4:]
+            else:
+                start_line = i-1
+                break
+            i += 1
+
+        # ヘッダ以降のデータを読み込む
+        self.angles = lines[start_line + IESreader.iesAngleLine][:-1].split(' ')
+        self.data = lines[start_line + IESreader.iesDataLine][:-1].split(' ')
+
+        print(self.angles)
+        print(self.data)
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # coding: utf-8
 
-from ANA_RC.anarc import *
 from ANA_DB.anadb import *
+from IES.iesReader import *
 
 if __name__ == "__main__":
     print("IESファイルを用いたシミュレーション ver.0.3")
     print("最適化アルゴリズム：ANA/DB")
 
-    Initial.set()
+    ies = IESreader()
 
-    ana = AnaDb()
+    # Initial.set()
 
-    ana.start()
+    # ana = AnaDb()
+
+    # ana.start()
